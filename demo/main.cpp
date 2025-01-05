6be4982d0036793fb76748b8b3ebf07211163db5
Michael Bethke <gibsonbethke@gmail.com>
2025-01-05 00:49:19 +0000

    Changed main to use SDL example of callbacks

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,45 +1,55 @@
+#define SDL_MAIN_USE_CALLBACKS 1 
 #include <SDL3/SDL.h>
-#include <iostream>
+#include <SDL3/SDL_main.h>
+
+static SDL_Window* window = nullptr;
+static SDL_Renderer* renderer = nullptr;
 
 
-int main(const int argc, const char* const argv[]) {
-	std::cout << "Hello, world" << std::endl;
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+	SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
 
-	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_Log("SDL_Init failed (%s)", SDL_GetError());
-		return 1;
+	if(!SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
 	}
 
-	SDL_Window *window = NULL;
-	SDL_Renderer *renderer = NULL;
-
-	if (!SDL_CreateWindowAndRenderer("SDL issue", 640, 480, 0, &window, &renderer)) {
-		SDL_Log("SDL_CreateWindowAndRenderer failed (%s)", SDL_GetError());
-		SDL_Quit();
-		return 1;
+	if(!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer)) {
+		SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
 	}
 
-	while (1) {
-		int finished = 0;
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_EVENT_QUIT) {
-				finished = 1;
-				break;
-			}
-		}
-		if (finished) {
-			break;
-		}
+	return SDL_APP_CONTINUE;
+}
 
-		SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+	if(event->type == SDL_EVENT_QUIT) {
+		return SDL_APP_SUCCESS;
 	}
+	return SDL_APP_CONTINUE;
+}
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
 
-	SDL_Quit();
-	return 0;
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+	const double now = ((double)SDL_GetTicks()) / 1000.0;
+
+	const float red = (float)(0.5 + 0.5 * SDL_sin(now));
+	const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+	const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+	SDL_SetRenderDrawColorFloat(renderer, red, green, blue, 1.0f);
+
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	return SDL_APP_CONTINUE;
+}
+
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+
 }
